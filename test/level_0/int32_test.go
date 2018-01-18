c37ffd1fabb9950b87a6250b68d376b427b78764
Tao Wen <taowen@gmail.com>
2018-01-18 15:47:12 +0800

    unmarshal int32

--- test/level_0/int32_test.go
+++ test/level_0/int32_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/thrift-iterator/go"
 	"github.com/thrift-iterator/go/test"
+	"github.com/v2pro/wombat"
 )
 
 func Test_decode_int32(t *testing.T) {
@@ -14,6 +15,18 @@ func Test_decode_int32(t *testing.T) {
 		proto.WriteI32(-1)
 		iter := c.CreateIterator(buf.Bytes())
 		should.Equal(int32(-1), iter.ReadInt32())
+	}
+}
+
+func Test_unmarshal_int32(t *testing.T) {
+	should := require.New(t)
+	for _, c := range test.Combinations {
+		buf, proto := c.CreateProtocol()
+		proto.WriteI32(-1)
+		var val int32
+		api := c.Config.Decode(wombat.Int32).Froze()
+		should.NoError(api.Unmarshal(buf.Bytes(), &val))
+		should.Equal(int32(-1), val)
 	}
 }
 
--- test/level_0/uint32_test.go
+++ test/level_0/uint32_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/thrift-iterator/go"
 	"github.com/thrift-iterator/go/test"
+	"github.com/v2pro/wombat"
 )
 
 func Test_decode_uint32(t *testing.T) {
@@ -14,6 +15,18 @@ func Test_decode_uint32(t *testing.T) {
 		proto.WriteI32(1024)
 		iter := c.CreateIterator(buf.Bytes())
 		should.Equal(uint32(1024), iter.ReadUint32())
+	}
+}
+
+func Test_unmarshal_uint32(t *testing.T) {
+	should := require.New(t)
+	for _, c := range test.Combinations {
+		buf, proto := c.CreateProtocol()
+		proto.WriteI32(1024)
+		var val uint32
+		api := c.Config.Decode(wombat.Uint32).Froze()
+		should.NoError(api.Unmarshal(buf.Bytes(), &val))
+		should.Equal(uint32(1024), val)
 	}
 }
 
