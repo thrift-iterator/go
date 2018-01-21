1f41f39eebed2b800b1260ccd7ad3ace4ccfd847
Tao Wen <taowen@gmail.com>
2018-01-21 14:38:26 +0800

    encode enum

---
+++ binding/static/encode_enum.go
@@ -0,0 +1,17 @@
+package static
+
+import (
+	"github.com/v2pro/wombat/generic"
+)
+
+func init() {
+	encodeAnything.ImportFunc(encodeEnum)
+}
+
+var encodeEnum = generic.DefineFunc(
+	"EncodeEnum(dst DT, src ST)").
+	Param("DT", "the dst type to copy into").
+	Param("ST", "the src type to copy from").
+	Source(`
+dst.WriteInt32(int32(src))
+	`)
